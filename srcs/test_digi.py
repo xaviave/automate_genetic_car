8804948c38f766614d71ba2e2444cd8bcbc12e2b
xaviave <hellxaviave@gmail.com>
2020-11-10 18:13:53 +0100

    Add little GA algo to find number list

--- srcs/test_digi.py
+++ srcs/test_digi.py
@@ -2,7 +2,7 @@ from test_algo.DigitalcodeFounder import DigitalcodeFounder
 
 
 def main():
-    f = DigitalcodeFounder(20, password_len=10, mutation=4)
+    f = DigitalcodeFounder(20, password_len=20, mutation=4)
     f.launch()
 
 
